25bb3778468b1f2d59fee1087bfc152d1f754576
Varun Singh <varunsingh00712@gmail.com>
2021-03-24 13:29:17 +0530

    Fresh Files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import "tailwindcss/tailwind.css"
+import logo from "./Static/images/sample.svg";
+function App() {
+  return (
+    <div className="">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
