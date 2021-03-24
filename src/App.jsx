7fbfbac0c5e46b64613a643fb9ecb9098202c9c7
Varun Singh <varunsingh00712@gmail.com>
2021-03-24 18:53:45 +0530

    Header Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
 import "tailwindcss/tailwind.css"
 import logo from "./Static/images/sample.svg";
+import Header from './Pages/HomePage/Header';
 function App() {
   return (
     <div className="">
-      <h1>Hello World</h1>
+      <Header/>
     </div>
   );
 }
