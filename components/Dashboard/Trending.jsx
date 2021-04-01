750bd46bb7f54e72694bb51e1f59a973acd20724
Varun Singh <varunsingh00712@gmail.com>
2021-04-01 21:42:16 +0530

    --UI Fixes Done

--- components/Dashboard/Trending.jsx
+++ components/Dashboard/Trending.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import style from "./Trending.module.css"
 
 const Trending = () => {
     return (
         <div>
             <h1>This is Trending Component</h1>
+            <div>
+                
+            </div>
         </div>
     )
 }
