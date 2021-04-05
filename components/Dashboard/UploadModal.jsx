8ac78d6bb16fb44c650904c393f710f7fb801a4c
Varun Singh <varunsingh00712@gmail.com>
2021-04-05 17:03:33 +0530

    --Styling Improved, Upload Modal

--- components/Dashboard/UploadModal.jsx
+++ components/Dashboard/UploadModal.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
-import styles from './UploadModal.module.css';
+import style from './UploadModal.module.css';
 const UploadModal = ({ user }) => {
   return (
     <div>
-      <input type="text" placeholder="Enter Name of Music" />
-      <input type="text" value={user} disabled />
-      <select>
-        <option defaultValue="Select Tags" disabled>
-          Select Tags
-        </option>
-        <option Value="Select Tags">Hip Hop</option>
-        <option Value="Select Tags">Base</option>
-        <option Value="Select Tags">Chill</option>
-        <option Value="Select Tags">Beats</option>
-        <option Value="Select Tags">Musical</option>
-        <option Value="Select Tags">Slow</option>
-        <option Value="Select Tags">EDM</option>
-        <option Value="Select Tags">Electric</option>
-        <option Value="Select Tags">Vocal</option>
-        <option Value="Select Tags">House</option>
-      </select>
+      <div className={style.form__row}>
+        <h1 style={{ color: 'wheat' }}>Login</h1>
+        <div className={style.inputs}>
+          <input type="text" placeholder="Enter Name of Music" />
+          <input type="text" value={user} disabled />
+          <select>
+            <option default>
+              Select Tags
+            </option>
+            <option Value="Select Tags">Hip Hop</option>
+            <option Value="Select Tags">Base</option>
+            <option Value="Select Tags">Chill</option>
+            <option Value="Select Tags">Beats</option>
+            <option Value="Select Tags">Musical</option>
+            <option Value="Select Tags">Slow</option>
+            <option Value="Select Tags">EDM</option>
+            <option Value="Select Tags">Electric</option>
+            <option Value="Select Tags">Vocal</option>
+            <option Value="Select Tags">House</option>
+          </select>
+        </div>
+      </div>
+      <div className={style.subscribe}>
+        <button className={style.subscribe__btn}>Upload</button>
+      </div>
     </div>
   );
 };
