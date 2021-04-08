e6ca0dba38d952b90560443ace47293afaa51718
kaustubh <kaustubh229@gmail.com>
2021-04-08 12:50:24 +0530

    Loader Fixed

--- components/Loader/Loader.jsx
+++ components/Loader/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { MagicSpinner } from "react-spinners-kit";
 const Loader = ({loading}) => {
     return (
-        <div style={{display:"flex",justifyContent:"center", zIndex:"1000", alignItems: 'center', padding: '50px', margin: '200px'}}>
+        <div style={{display:"flex",justifyContent:"center", zIndex:"1000", padding: '50px', alignItems: 'center'}}>
             <MagicSpinner size={50} color="gray" loading={loading} />
         </div>
        
